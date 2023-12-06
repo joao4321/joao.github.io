b2a82608ecc3b501a220f197fb1b75f7603a94b4
joao4321 <89591695+joao4321@users.noreply.github.com>
2023-12-05 23:52:55 -0300

    Update relogio.js

--- relogio.js
+++ relogio.js
@@ -1,21 +1,20 @@
-var palavra = document.getElementById("verificar");
-palavra.addEventListener("click", ePalindromo, false);
-
-function ePalindromo() {
-    let texto = document.getElementById('texto').value;
-    let textoSemEspaco = texto.replace(" ", "").toLowerCase();
-    let textInvertido = inverter(textoSemEspaco);
-
-    if (textoSemEspaco == textInvertido)
-        alert(texto + " é um palíndromo!") 
-    else
-        alert(texto + " não é um palíndromo") 
+function atualizarRelogio() {
+    var relogio = document.getElementById('relogio');
+    
+    var dataHoraAtual = new Date();
+    var horas = adicionarZero(dataHoraAtual.getHours());
+    var minutos = adicionarZero(dataHoraAtual.getMinutes());
+    var segundos = adicionarZero(dataHoraAtual.getSeconds());
+    
+    relogio.innerText = `${horas}:${minutos}:${segundos}`;
 }
 
-function inverter(str) {
-    var newString = "";
-    for (var i = str.length - 1; i >= 0; i--) { 
-        newString += str[i];
-    }
-    return newString;
+function adicionarZero(numero) {
+    return numero < 10 ? '0' + numero : numero;
 }
+
+// Atualizar o relógio a cada segundo
+setInterval(atualizarRelogio, 1000);
+
+// Atualizar o relógio imediatamente ao carregar a página
+window.onload = atualizarRelogio;
